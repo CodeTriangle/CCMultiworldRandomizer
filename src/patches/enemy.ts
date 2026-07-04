45a6ea57dff7f3f2ef02040dc9a74411857a55c9
Richard Snider <lighthousemaniac@gmail.com>
2026-07-04 01:47:01 -0600

    send killsanity checks

---
+++ src/patches/enemy.ts
@@ -0,0 +1,15 @@
+import type MwRandomizer from "../plugin";
+
+export function patch(plugin: MwRandomizer) {
+	sc.Combat.inject({
+		notifyCombatantDefeated(combatant, resolveDefeat, notifyArena) {
+			this.parent(combatant, resolveDefeat, notifyArena);
+			if (combatant instanceof ig.ENTITY.Enemy) {
+				let enemy = sc.randoData.enemies[combatant.enemyName];
+				if (enemy) {
+					sc.multiworld.reallyCheckLocation(enemy.kill);
+				}
+			}
+		}
+	});
+}
