19a08f21a8fe0e28edddabc81af04a1812902db2
Richard Snider <lighthousemaniac@gmail.com>
2026-07-05 17:27:04 -0600

    only notify the server once

--- src/patches/enemy.ts
+++ src/patches/enemy.ts
@@ -6,7 +6,7 @@ export function patch(plugin: MwRandomizer) {
 			this.parent(combatant, resolveDefeat, notifyArena);
 			if (combatant instanceof ig.ENTITY.Enemy) {
 				let enemy = sc.randoData.enemies[combatant.enemyName];
-				if (enemy) {
+				if (enemy && !sc.multiworld.localCheckedLocations.has(enemy.kill)) {
 					sc.multiworld.reallyCheckLocation(enemy.kill);
 				}
 			}
